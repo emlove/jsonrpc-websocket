c4461c93e5ac273a65aa59fef6e746d5d6f3d2a0
Valentin Lab <valentin.lab@kalysto.org>
2015-04-07 09:11:30 +0800

    chg: test: only count non-empty lines when testing for 100-lines hard limit.

--- tests.py
+++ tests.py
@@ -40,7 +40,8 @@ class TestJSONRPCClient(TestCase):
     def test_length(self):
         """Verify that this library is really smaller than 100 lines, as stated in README.rst"""
         with open(inspect.getfile(Server)) as library_file:
-            self.assertLessEqual(len(library_file.readlines()), 100)
+            self.assertLessEqual(len([l for l in library_file.readlines()
+                                      if l != "\n"]), 100)
 
     def test_pep8_conformance(self):
         """Test that we conform to PEP8."""
